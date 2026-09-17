e0a1c9fc98bb51d0876ca234c8949510726bd3e4
Michael Taylor <michael@Michaels-MacBook-Pro.local>
2026-09-16 21:24:12 -0500

    Holy finally changed the normal -> insert mode behavior in zsh where the backspace can't delete before the cursor position

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,6 +1,10 @@
 # Enable Vim Keybindings
 bindkey -v
 
+# Let Backspace delete text from before insert mode was entered
+bindkey -M viins '^?' backward-delete-char
+bindkey -M viins '^H' backward-delete-char
+
 # Cursor Customization
 print -n "\e[2 q"
 
